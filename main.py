df027082b4e7a91b4531c2c6ee05a5c79bbd736a
Marcel Waterlander <112943380+o11899nine@users.noreply.github.com>
2023-09-28 09:20:53 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG, format=" %(asctime)s -  %(levelname)s -  %(message)s"
+)
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
