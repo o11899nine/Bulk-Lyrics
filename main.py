c672d413277640db63c7915b5032e1562a8218fc
Marcel Waterlander <112943380+o11899nine@users.noreply.github.com>
2023-09-29 21:35:40 +0200

    Show first google hit when song not found

--- main.py
+++ main.py
@@ -17,22 +17,21 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import re
-from docx import Document
+import docx
+from docx.shared import RGBColor
 import pyperclip
 
 
 # TODO: Opmaak van doc
 # TODO: Browser compatibility
 # TODO: Tkinter GUI
-# TODO: Song niet gevonden: save link to first hit, show links
-# TODO: "liedje vna" weg
 
 def main() -> None:
     songlist: list = get_songlist()
 
     print("Setting up...")
     driver = initiate_driver()
-    document = Document()
+    document = docx.Document()
 
     for user_song in songlist:
         print(f"Fetching info for {user_song}")
@@ -51,31 +50,36 @@ def main() -> None:
 
 def get_song_data(user_song: str, soup: BeautifulSoup) -> dict:
     '''
-    Finds a song's title, artist and lyrics in the song's BeautifulSoup,
-    If a song's lyrics are not found, the user's input is used for the song's title
+    Finds a song's title, artist and lyrics in the song's BeautifulSoup and 
+    returns a dict with that info. If a song's lyrics are not found, the user's 
+    input is used for the song's title and google's first hit for the song's 
+    lyrics is stored in the dict
     '''
 
     lyrics: ResultSet = soup.find_all("div", {"jsname": "U8S5sf"})
 
     if len(lyrics) == 0:
-        title: str = user_song
-        artist: bool = False
-        lyrics: bool = False
-        
+        title = user_song
+        artist = False
+        lyrics = False
+
     else:
         title: str = soup.find("div", {"data-attrid": "title"}).text
         artist: str = soup.find("div", {"data-attrid": "subtitle"}).text
         artist = delete_extra_text(artist)
+    
+    first_google_hit = soup.find("a", {"jsname": "UWckNb"})["href"]
 
-    return {"title": title, "artist": artist, "lyrics": lyrics}
+    return {"title": title, "artist": artist, "lyrics": lyrics, "link": first_google_hit}
+
 
 def delete_extra_text(artist: str) -> str:
     """Deletes the words 'Song by' before the artist. Then returns the artist"""
     # Google displays the artist as "Song by Artist", so the second uppercase
     # letter is the start of the artist's name. The code below finds the index
     # of that second uppercase letter and then removes all text before it
-    m = re.search(r'^([^A-Z]*[A-Z]){2}', artist)
-    idx = m.span()[1]-1
+    m: re.Match = re.search(r'^([^A-Z]*[A-Z]){2}', artist)
+    idx: int = m.span()[1]-1
     return artist[idx:]
 
 
@@ -86,7 +90,7 @@ def add_song_to_doc(data: dict, doc) -> None:
 
     if data["artist"]:
         doc.add_paragraph().add_run(data["artist"]).bold = True
-
+        
     if data["lyrics"]:
         for paragraph in data["lyrics"]:
             lines: ResultSet = paragraph.find_all("span", {"jsname": "YS01Ge"})
@@ -96,7 +100,78 @@ def add_song_to_doc(data: dict, doc) -> None:
                 if line != lines[-1]:
                     p.add_run("\n")
     else:
-        doc.add_paragraph().add_run("Lyrics Not Found")
+        doc.add_paragraph().add_run("Lyrics Not Found").font.color.rgb = RGBColor(255, 0, 0)
+        p = doc.add_paragraph()
+        p.add_run(f"You might find them here:")
+        p.add_run("\n")
+        add_hyperlink(p, data["link"], data["link"])
+
+
+def add_hyperlink(paragraph, text, url):
+    """
+    Source:
+    https://stackoverflow.com/questions/47666642/adding-an-hyperlink-in-msword-by-using-python-docx
+    """
+    # This gets access to the document.xml.rels file and gets a new relation id value
+    part = paragraph.part
+    r_id = part.relate_to(
+        url, docx.opc.constants.RELATIONSHIP_TYPE.HYPERLINK, is_external=True)
+
+    # Create the w:hyperlink tag and add needed values
+    hyperlink = docx.oxml.shared.OxmlElement('w:hyperlink')
+    hyperlink.set(docx.oxml.shared.qn('r:id'), r_id, )
+
+    # Create a new run object (a wrapper over a 'w:r' element)
+    new_run = docx.text.run.Run(
+        docx.oxml.shared.OxmlElement('w:r'), paragraph)
+    new_run.text = text
+
+    # Set the run's style to the builtin hyperlink style, defining it if necessary
+    new_run.style = get_or_create_hyperlink_style(part.document)
+    # Alternatively, set the run's formatting explicitly
+    # new_run.font.color.rgb = docx.shared.RGBColor(0, 0, 255)
+    # new_run.font.underline = True
+
+    # Join all the xml elements together
+    hyperlink.append(new_run._element)
+    paragraph._p.append(hyperlink)
+    return hyperlink
+
+# This is only needed if you're using the builtin style above
+
+
+def get_or_create_hyperlink_style(d):
+    """
+    Source: 
+    https://stackoverflow.com/questions/47666642/adding-an-hyperlink-in-msword-by-using-python-docx
+
+    If this document had no hyperlinks so far, the builtin
+       Hyperlink style will likely be missing and we need to add it.
+       There's no predefined value, different Word versions
+       define it differently.
+       This version is how Word 2019 defines it in the
+       default theme, excluding a theme reference.
+    """
+    if "Hyperlink" not in d.styles:
+        if "Default Character Font" not in d.styles:
+            ds = d.styles.add_style("Default Character Font",
+                                    docx.enum.style.WD_STYLE_TYPE.CHARACTER,
+                                    True)
+            ds.element.set(docx.oxml.shared.qn('w:default'), "1")
+            ds.priority = 1
+            ds.hidden = True
+            ds.unhide_when_used = True
+            del ds
+        hs = d.styles.add_style("Hyperlink",
+                                docx.enum.style.WD_STYLE_TYPE.CHARACTER,
+                                True)
+        hs.base_style = d.styles["Default Character Font"]
+        hs.unhide_when_used = True
+        hs.font.color.rgb = docx.shared.RGBColor(0x05, 0x63, 0xC1)
+        hs.font.underline = True
+        del hs
+
+    return "Hyperlink"
 
 
 def fetch_song_soup(song: str, driver) -> BeautifulSoup:
